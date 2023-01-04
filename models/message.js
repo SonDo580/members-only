e9741bd8d56a5b96fd90ee882759f09b6b449bcb
Son Do <dominhson580@gmail.com>
2023-01-04 17:18:30 +0700

    Define MessageSchema

---
+++ models/message.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true, maxLength: 300 },
+  content: { type: String, required: true },
+  timestamps: true,
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+});
