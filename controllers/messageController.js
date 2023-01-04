4ca3263680f180fd16710191044b56910766d78f
Son Do <dominhson580@gmail.com>
2023-01-04 17:36:26 +0700

    Add controller functions

---
+++ controllers/messageController.js
@@ -0,0 +1,15 @@
+exports.message_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: Home Page");
+};
+
+exports.message_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Create Messate GET");
+};
+
+exports.message_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Create Messate POST");
+};
+
+exports.message_delete = (req, res) => {
+  res.send("NOT IMPLEMENTED: Delete Messate");
+};
