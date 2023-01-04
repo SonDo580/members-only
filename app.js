d3d10979beb71c27665316612500c6a7088a5076
Son Do <dominhson580@gmail.com>
2023-01-04 11:17:19 +0700

    Connect to MongoDB

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
