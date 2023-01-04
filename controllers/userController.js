13e41594042278651c8cda323940493abcb4169d
Son Do <dominhson580@gmail.com>
2023-01-04 17:42:09 +0700

    Add controller functions

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+exports.sign_up_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Sign Up GET");
+};
+
+exports.sign_up_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Sign Up POST");
+};
+
+exports.log_in_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Log In GET");
+};
+
+exports.log_in_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Log In POST");
+};
+
+exports.become_member_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Become Member GET");
+};
+
+exports.become_member_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Become Member POST");
+};
