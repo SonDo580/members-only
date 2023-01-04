7db3658050c72fecd739f92237ed28fdfed804da
Son Do <dominhson580@gmail.com>
2023-01-04 17:23:31 +0700

    Export model 'Message'

--- models/message.js
+++ models/message.js
@@ -11,3 +11,5 @@ const MessageSchema = new Schema({
 MessageSchema.virtual("url").get(function () {
   return `/message/${this._id}`;
 });
+
+module.exports = mongoose.model("Message", MessageSchema);
