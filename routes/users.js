ba18491125ead0772849780183555987fc5e845b
Son Do <dominhson580@gmail.com>
2023-01-04 17:51:32 +0700

    Define user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+const userController = require("../controllers/userController");
+
+router.get("/signup", userController.sign_up_get);
+router.post("/signup", userController.sign_up_post);
+router.get("/login", userController.log_in_get);
+router.post("/login", userController.log_in_post);
+router.get("/become-member", userController.become_member_get);
+router.post("/become-member", userController.become_member_post);
 
 module.exports = router;
