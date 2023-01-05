cc929f389fba8171a9801f691d0e7ccf6715fe28
Son Do <dominhson580@gmail.com>
2023-01-05 16:16:28 +0700

    Validate user input (server side)
    Haven't check: passwords match, unique username

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,9 +5,38 @@ exports.sign_up_get = (req, res) => {
   res.render("signup_form", { title: "Sign Up" });
 };
 
-exports.sign_up_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Sign Up POST");
-};
+exports.sign_up_post = [
+  // Validate user input
+  body("firstName")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified")
+    .matches("^[A-Za-z]+$")
+    .withMessage("First name must contain only letters (no spaces)"),
+  body("lastName")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Last name must be specified")
+    .matches("^[A-Za-z]+$")
+    .withMessage("Last name must contain only letters (no spaces)"),
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Username must be specified")
+    .matches("^[A-Za-z0-9]+$")
+    .withMessage("Username must contain only letters and digits (no spaces)"),
+  body("password")
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Password must be specified"),
+  body("confirmPassword")
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Password confirm must be specified"),
+];
 
 exports.log_in_get = (req, res) => {
   res.render("login_form", { title: "Log In" });
