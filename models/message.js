c8e3720a05b6ec5a5809ce9f7b4b532435929ff5
Son Do <dominhson580@gmail.com>
2023-01-07 14:45:50 +0700

    Fix Message Schema

--- models/message.js
+++ models/message.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const MessageSchema = new Schema({
-  title: { type: String, required: true, maxLength: 300 },
-  content: { type: String, required: true },
-  timestamps: true,
-  author: { type: Schema.Types.ObjectId, ref: "User" },
-});
+const MessageSchema = new Schema(
+  {
+    title: { type: String, required: true, maxLength: 300 },
+    content: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
 
 MessageSchema.virtual("url").get(function () {
   return `/message/${this._id}`;
