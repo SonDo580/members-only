cd9938ea578b715421588b10750e0815469f4e54
Son Do <dominhson580@gmail.com>
2023-01-07 23:07:45 +0700

    User router.route()

--- routes/message.js
+++ routes/message.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const messageController = require("../controllers/messageController");
 
 router.get("/", messageController.message_list);
-router.get("/create", messageController.message_create_get);
-router.post("/create", messageController.message_create_post);
+
+router
+  .route("/create")
+  .get(messageController.message_create_get)
+  .post(messageController.message_create_post);
+
 router.delete("/:id", messageController.message_delete);
 
 module.exports = router;
