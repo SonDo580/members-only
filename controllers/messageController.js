9ba715356802e70aa9bff28535e8e755fb015019
Son Do <dominhson580@gmail.com>
2023-01-07 15:06:13 +0700

    Define message_create_post

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,7 @@
 const Message = require("../models/message");
 
+const { body, validationResult } = require("express-validator");
+
 exports.message_list = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Home Page");
 };
@@ -7,12 +9,54 @@ exports.message_list = (req, res, next) => {
 exports.message_create_get = (req, res) => {
   res.render("message_form", {
     title: "Create Message",
+    oldData: null,
+    errors: null,
   });
 };
 
-exports.message_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Create Message POST");
-};
+exports.message_create_post = [
+  // Validate and sanitize input
+  body("title")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Title must be specified"),
+  body("lastName")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Content must be specified"),
+
+  // Process request
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Render the form again if there are errors
+    if (!errors.isEmpty()) {
+      return res.render("message_form", {
+        title: "Create Message",
+        oldData: req.body,
+        errors: errors.array(),
+      });
+    }
+
+    // Data is valid
+
+    const message = new Message({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.user,
+    });
+
+    message.save((err) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.redirect("/message");
+    });
+  },
+];
 
 exports.message_delete = (req, res) => {
   res.send("NOT IMPLEMENTED: Delete Message");
