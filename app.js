bb9139b4e158d34e23f7d283f0f1b9c0930f38cf
Son Do <dominhson580@gmail.com>
2023-01-08 00:04:09 +0700

    Define local variable currentUser

--- app.js
+++ app.js
@@ -67,6 +67,11 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.use("/", indexRouter);
 app.use("/user", userRouter);
 app.use("/message", messageRouter);
