55792655ec2bc09158446be144aefaf870bf022f
Son Do <dominhson580@gmail.com>
2023-01-07 23:10:16 +0700

    Use router.route()

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,23 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
-router.get("/signup", userController.sign_up_get);
-router.post("/signup", userController.sign_up_post);
-router.get("/login", userController.log_in_get);
-router.post("/login", userController.log_in_post);
+router
+  .route("/signup")
+  .get(userController.sign_up_get)
+  .post(userController.sign_up_post);
+
+router
+  .route("/login")
+  .get(userController.log_in_get)
+  .post(userController.log_in_post);
+
 router.get("/logout", userController.log_out);
-router.get("/become-member", userController.become_member_get);
-router.post("/become-member", userController.become_member_post);
+
+router
+  .route("/become-member")
+  .get(userController.become_member_get)
+  .post(userController.become_member_post);
+
 router.get("/check-unique/:username", userController.check_unique);
 
 module.exports = router;
