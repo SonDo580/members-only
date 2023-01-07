fb181b5a14af3a65baf5db09b10510ddda383268
Son Do <dominhson580@gmail.com>
2023-01-07 22:04:30 +0700

    Move passport configs to below 'express.static...'

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
 passport.use(
   new LocalStrategy((username, password, done) => {
     User.findOne({ username: username }, (err, user) => {
@@ -56,12 +62,6 @@ passport.deserializeUser((id, done) => {
     done(err, user);
   });
 });
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
